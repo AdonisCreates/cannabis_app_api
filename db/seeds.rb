1369a858195422d5281a45dc0e741533b5125134
Adriel Adonis <arodriguez32@fisher.edu>
2020-09-24 16:01:38 -0400

    it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 100.times do
+#     Weed.create(
+#       strain: Faker::Cannabis.strain,
+#       type: Faker::Cannabis.type,
+#       brand: Faker::Cannabis.brand
+#     )
+#   end
 
-100.times do
-    Weed.create(
-      strain: Faker::Hipster.sentence(word_count: 1),
-      type: Faker::Cannabis.type,
-      brand: Faker::Cannabis.brand
-    )
-  end
+# puts "Seeded database"
 
-puts "Seeded database"
+Weed.create([{
+  strain: 'bluedreams',
+  brand: 'Northern Lights'
+}])
